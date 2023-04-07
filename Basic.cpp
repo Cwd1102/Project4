a7d4a3d33df0f0ef583365a18d439b4aa498e55b
Ouwen Dai <ouwen@gmail.com>
2023-04-07 02:12:34 -0400

    4/7 2:12AM

--- Basic.cpp
+++ Basic.cpp
@@ -4,26 +4,32 @@
 // Description: Creates a new Basic Monkey
 // Preconditions: None
 // Postconditions: Can create a Basic Monkey
-Basic(){}
+Basic::Basic(){}
 // Name: Basic(int health, int location)
 // Description: Creates a new basic bloon with health at a specific location
 // if health = 1, red, 2=blue, 3=green, 4=yellow, 5=pink, >=6=black
 // Preconditions: None
 // Postconditions: Can create a basic bloon
-Basic(int, int){}
+Basic::Basic(int, int){}
 // Name: ~Basic - Virtual Destructor
 // Description: Makes sure everything in child class is deallocated
 // Preconditions: None
 // Postconditions: Everything dynamically allocated is deallocated
-~Basic(){}
+Basic::~Basic(){}
 // Name: Pop
 // Description: Changes the color of the bloon based on how many health it has left
 // red = 1, blue = 2, green = 3, yellow = 4, pink = 5, black >=6
 // Preconditions: None
 // Postconditions: Returns 1 if still alive, returns 0 if health <= 0
-int Pop(int damage){}
+int Basic::Pop(int damage){
+
+    return 0;
+}
 // Name: GetColor()
 // Description: Returns the current color of a bloon
 // Preconditions: None
 // Postconditions: None
-string GetColor(){}
+string Basic::GetColor(){
+
+    return "";
+}
--- Bloon.cpp
+++ Bloon.cpp
@@ -1,2 +1,64 @@
 #include "Bloon.h"
 
+// Name: Bloon() - Default Constructor
+// Description: Abstract class - cannot directly instantiate this class
+// Preconditions: None
+// Postconditions: Populates variables used in child classes (may not be specifically used)
+Bloon::Bloon(){}
+// Name: Bloon(int health, int location) - Overloaded Constructor
+// Description: Abstract class - cannot directly instantiate this class
+// Preconditions: None
+// Postconditions: Populates variables used in child classes
+Bloon::Bloon(int health, int location){}
+// Name: virtual ~Bloon - Virtual Destructor
+// Description: Makes sure everything in child class is deallocated
+// Preconditions: None
+// Postconditions: Everything dynamically allocated is deallocated
+Bloon::~Bloon(){}
+// Name: GetHealth()
+// Description: Returns health of bloon
+// Preconditions: Bloon has health
+// Postconditions: None
+int Bloon::GetHealth(){
+    return 0;
+}
+// Name: SetHealth()
+// Description: Sets health of bloon
+// Preconditions: None
+// Postconditions: Health is set
+void Bloon::SetHealth(int){}
+// Name: GetLocation()
+// Description: Returns location of bloon
+// Preconditions: Bloon has location
+// Postconditions: None
+int Bloon::GetLocation(){
+
+    return 0;
+}
+// Name: SetLocation()
+// Description: Sets location of bloon
+// Preconditions: None
+// Postconditions: Location is set
+void Bloon::SetLocation(int){}
+// Name: Pop()
+// Description: Defines popping
+// Preconditions: None
+// Postconditions: None (Purely virtual - must be in child classes)
+int Bloon::Pop(int damage){
+    return 0;
+}
+// Name: GetColor
+// Description: Virtual
+// Preconditions: None
+// Postconditions: None (Purely virtual - must be in child classes)
+string Bloon::GetColor(){
+
+    return " ";
+}
+// Name: Overloaded <<
+// Description: Prints the details of an bloon
+// Preconditions: all variables are set valid
+// Postconditions: returns an ostream with output of bloon
+ostream& operator<<(ostream&, Bloon&){
+
+}
